98c8fbee5d6dfdf9030f66b1159b60d93d7d1213
Bo Henderson <bohende@gmail.com>
2019-08-25 02:34:13 +0200

    almost get payment streams working

--- screens/ConnectScreen.js
+++ screens/ConnectScreen.js
@@ -1,3 +1,4 @@
+import { ethers as eth } from "ethers";
 import React from 'react';
 import {
   StyleSheet,
@@ -6,52 +7,122 @@ import {
   FlatList,
   TouchableOpacity,
 } from 'react-native';
+const timer = require('react-native-timer');
 import ConnectingStatus from '../components/ConnetingStatus';
 
+import { toBN } from '../lib/bn';
 import { getChannel } from '../lib/channel';
 import { micropay } from '../lib/micropay';
 
+const streamName = 'PaymentStream'
+const { formatEther, parseEther } = eth.utils
+
 export default class ConnectScreen extends React.Component {
   state = {
-    wifiList: [],
-    connectingToSsid: '',
-    recipient: '0xd0cfb387bb1874d12a1a1399dcb527f7b0b13efe',
+    balance: '?.??',
+    channel: null,
+    connectedToSsid: '',
     paymentAmount: '0.01',
+    recipient: '',
+    streaming: false,
+    wifiList: [],
   };
 
   componentDidMount = () => {
     this.loadAvailableWifiList();
-    getChannel();
+    this.handleButtonCall.bind(this)
+    this.getBalance()
   }
 
+  getBalance = async () => {
+    const channel = await getChannel()
+    const tokenBalance = (await channel.getState()).persistent.channel.balanceTokenUser
+    this.setState({ balance: formatEther(tokenBalance) })
+    return tokenBalance;
+  }
+
+  // TODO: write this for real
+  getProviderAddress = (ssid) => {
+    this.setState({ recipient: '0xd0cfb387bb1874d12a1a1399dcb527f7b0b13efe' })
+  }
+
+  // TODO: write this for real
+  getDataUsage = () => {
+    return Date.now()
+  }
+
+  // TODO: write this for real
   loadAvailableWifiList = () => {
-    // TODO: load list
-    this.setState({
-      wifiList: [
-        { key: 'YOyo-wifi', ssid: 'YOyo-wifi', price: 7 },
-        { key: 'ple4se d0nt', ssid: 'ple4se d0nt', price: 3 },
-        { key: 'hello!', ssid: 'hello!', price: 5 },
-      ]
-    });
+    this.setState({ wifiList: [
+      { key: 'YOyo-wifi', ssid: 'YOyo-wifi', price: 7 },
+      { key: 'ple4se d0nt', ssid: 'ple4se d0nt', price: 3 },
+      { key: 'hello!', ssid: 'hello!', price: 5 },
+    ] });
   }
 
   handleButtonCall = (newSsid) => {
-    this.setState({ connectingToSsid: newSsid });
+    // start stream?
+    console.log(`Connecting to: ${newSsid}`)
+    this.setState({ connectedToSsid: newSsid });
+    if (newSsid) {
+      this.startPaymentStream()
+    } else {
+      this.stopPaymentStream()
+    }
+  }
+
+  getProviderAddress = () => {
+    this.setState({ recipient: '0xd0cfb387bb1874d12a1a1399dcb527f7b0b13efe' })
+  }
+
+  startPaymentStream = () => {
+    const { paymentAmount, recipient } = this.state
+    this.channel = getChannel();
+    console.log(`Starting payment stream`)
+    // TODO: get data usage, calculate charge
+    timer.setInterval(
+      streamName,
+      async () => {
+        console.log(`Sending ${parseEther(paymentAmount)} to ${recipient}`)
+        await micropay(paymentAmount, recipient)
+        console.log(`Success! (hopefully)`)
+      },
+      3000,
+    )
+  }
+
+  stopPaymentStream = () => {
+    console.log(`Stopping payment stream`)
+    timer.clearInterval(streamName)
   }
 
   render = () => {
-    const { connectingToSsid, paymentAmount, recipient } = this.state;
+    const { balance, connectedToSsid, paymentAmount, recipient } = this.state;
     return (
       <View style={styles.container}>
 
         <View style={styles.container}>
+          <Text style={styles.titleText}>
+            Balance: {balance}{'\n'}
+          </Text>
           <Text style={styles.titleText}>
             List of Available Wi-Fi
           </Text>
           <View style={styles.container}>
             <FlatList
               data={this.state.wifiList}
-              renderItem={({ item }) => <ConnectingStatus item={item} reset={item.ssid !== connectingToSsid} handleButtonCall={this.handleButtonCall} />}
+              extraData={this.state}
+              renderItem={({ item }) => {
+                if (connectedToSsid && connectedToSsid !== item.ssid) {
+                  return null;
+                } else {
+                  return (<ConnectingStatus
+                    item={item}
+                    reset={item.ssid !== connectedToSsid}
+                    handleButtonCall={this.handleButtonCall}
+                  />)
+                }
+              }}
             />
           </View>
         </View>
